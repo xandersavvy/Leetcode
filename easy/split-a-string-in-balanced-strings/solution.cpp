89ff337ebd2f3f85d4a5803339fb76a1c6bfa9ac
Souvik Ghosh <54327571+xandersavvy@users.noreply.github.com>
2021-03-11 00:49:34 +0530

    Create solution.cpp

---
+++ easy/split-a-string-in-balanced-strings/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int cnt = 0;
+         int ans = 0;
+         for(int i =0;i<s.size();i++){
+            cnt = 0;
+            for(int j = i;j<s.size();j++){
+               if(s[j] == 'R')cnt++;
+               else cnt--;
+               if(j-i>0 && cnt == 0 ){
+                  ans++;
+                  i=j;
+                  break;
+               }
+            }
+         }
+         return ans;
+    }
+};
+//0 ms	6.2 MB
