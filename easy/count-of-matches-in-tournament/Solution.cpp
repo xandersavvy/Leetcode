5028c977156acf5fd0fa7342334b32722766410c
Souvik Ghosh <xandersavy@gmail.com>
2021-05-06 16:23:51 +0530

    Create Solution.cpp

---
+++ easy/count-of-matches-in-tournament/Solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+       int count = 0;
+        while(n>2)
+            if(n%2==0) {
+                count+=n/2;
+                n=n/2;
+                }
+            else{
+                count+=(n-1)/2;
+                n=(n-1)/2+1;
+            }
+        return (n>1)?++count:0;
+    }
+};
