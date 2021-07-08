640d08f0397fedb5cc4fc4e0c2457f70412ada83
Souvik Ghosh <xandersavy@gmail.com>
2021-07-09 01:24:45 +0530

    Create solution.cpp

---
+++ easy/search-insert-position/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        if(binary_search(nums.begin(), nums.end(), target))
+        {
+            vector<int>::iterator it;
+            it = find(nums.begin(), nums.end(), target);
+            return it - nums.begin();
+        }
+        else return (lower_bound(nums.begin(), nums.end(), target) - nums.begin());
+    }
+};
